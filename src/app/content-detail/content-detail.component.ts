200609c4fe3d67655056b90f276f67c055dd30c8
Philip Rosen <w0740881@myscc.ca>
2021-11-30 21:52:25 -0500

    Routing added

--- src/app/content-detail/content-detail.component.ts
+++ src/app/content-detail/content-detail.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Content} from "../helper-files/content-interface";
+import {Observable} from "rxjs";
+import { ContentServiceService } from '../services/content-service.service';
 
 @Component({
   selector: 'app-content-detail',
   templateUrl: './content-detail.component.html',
   styleUrls: ['./content-detail.component.scss']
 })
-export class ContentDetailComponent implements OnInit {
+  export class ContentDetailComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  id: number;
+  //content: Content;
+  constructor( private route: ActivatedRoute, private content: ContentServiceService) {}
+
+  ngOnInit(){
+    this.route.paramMap.subscribe(params => {
+      this.id = +params.get('id');
+
+      this.content.getContent(this.id).subscribe(
+        c => this.content = c);
+          console.log(this.content);
+
+        });
+  }
+
+
+
+  getContentItem({id}: { id: any }): Observable<Content>{ console.log("Retrieving Content");
+        return this.getContentItem({id: "api/content/" + id});
   }
 
 }
